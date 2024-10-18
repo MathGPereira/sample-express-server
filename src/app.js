2b23cc960785b90ab09919b6183766d709b145f8
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-10-18 15:08:14 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import newsRouter from './routes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: '*'
+}))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(
